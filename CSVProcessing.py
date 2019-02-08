cbaa4eaa40223f1e3dc84704ad5e532b9c790c5e
Geoffroy Penny <geoffroypenny@hotmail.com>
2019-02-08 11:50:10 -0500

    Added comments and figured out how to get the mean of a speicfic row by slicing

--- CSVProcessing.py
+++ CSVProcessing.py
@@ -1,19 +1,24 @@
 import pandas as pd
 
+# These are the files we will be reading
+# TODO: scan directory for CSV files and use those by default
 data_set_1 = 'cars.csv'
 data_set_2 = 'head.injury.csv'
 
+# Reads the CSV files with pandas
 DS1 = pd.read_csv(data_set_1)
 DS2 = pd.read_csv(data_set_2)
-
-categorical = False
-numerical = False
 
 print("Data Set 1: ")
 print('Columns', len(DS1.columns))
 print('Rows', len(DS1.values))
 print('First row of data: ', DS1.values[1])
 
+# Booleans used in determining what types of data are in the columns
+categorical = False
+numerical = False
+
+# Loops through the first row to determine what kind of data we are looking at
 for i in range(len(DS1.values[1])):
     if isinstance(DS1.values[1][i], str):
         print('Column', i, 'is categorical')
@@ -22,7 +27,7 @@ for i in range(len(DS1.values[1])):
         print('Column', i, 'is numerical')
         numerical = True
 
-
+# If statements to check if the values are numerical, categorical or both
 if categorical and not numerical:
     print("The data is categorical")
 if not categorical and numerical:
@@ -30,8 +35,15 @@ if not categorical and numerical:
 if categorical and numerical:
     print("The data is both numerical and categorical")
 
-print(DS1.mean())
-print(DS1.mode())
+# Finds the mean for each column
+# TODO: if categorical: do not find mean, find mode!
+for i in range(len(DS1.columns)):
+    DS1_Sliced = DS1.iloc[:,i:i+1]
+    print('Column', i, 'mean is:', DS1_Sliced.mean())
 
-#for c in DS1.columns:
-#    DS1[c].fillna()
+
+
+# print('Mean of numerical columns:\n', DS1.mean())
+
+# print(DS1.mode(dropna=False))
+
